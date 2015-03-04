ede83b2fb175b39b57a0cd4a9bd1f90e7632d16a
Njål Eide <njal.eide@evry.com>
2015-03-04 08:47:11 +0100

    Startup delayed until device ready

---
+++ Projects/Statens Vegvesen/controllers/setupController.js
@@ -0,0 +1,42 @@
+﻿(function ()
+{
+    "use strict";
+
+    var app = angular.module("vegvesenApp");
+
+    app.controller("setupController", function ($scope, $location)
+    {
+        document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+        function onDeviceReady()
+        {
+            // Handle the Cordova pause and resume events
+            document.addEventListener('pause', onPause.bind(this), false);
+            document.addEventListener('resume', onResume.bind(this), false);
+
+            $scope.$apply(function() {$location.path("/startup");});
+        };
+
+
+
+
+        //var test = fileService.ReadSettingsfile(null, function () { console.log("Failed to open settings file"); });
+        //console.log(test);
+    });
+
+
+
+
+    function onPause()
+    {
+        // TODO: This application has been suspended. Save application state here.
+        console.log("Paused");
+    };
+
+    function onResume()
+    {
+        // TODO: This application has been reactivated. Restore application state here.
+        console.log("Resume");
+    };
+
+})();
--- Projects/Statens Vegvesen/scripts/index.js
+++ Projects/Statens Vegvesen/scripts/index.js
@@ -2,22 +2,47 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
-(function () {
+(function ()
+{
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    var app = angular.module("vegvesenApp", ["ngRoute", "fileServices"]);
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-    };
 
-    function onPause() {
+    app.config(function ($routeProvider)
+    {
+        $routeProvider.when("/setup", { templateUrl: "views/setup.html" });
+        $routeProvider.when("/startup", { templateUrl: "views/startup.html" });
+        $routeProvider.otherwise({ templateUrl: "views/setup.html" });
+
+    });
+
+    app.controller("mainController", function ($scope, $location, fileService)
+    {
+        //var test = fileService.ReadSettingsfile(null, function () { console.log("Failed to open settings file"); });
+        //console.log(test);
+
+        document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+        function onDeviceReady()
+        {
+            // Handle the Cordova pause and resume events
+            document.addEventListener('pause', onPause.bind(this), false);
+            document.addEventListener('resume', onResume.bind(this), false);
+
+            $location.path("/startup");
+            console.log($location.path());
+        };
+    });
+    
+
+    function onPause()
+    {
         // TODO: This application has been suspended. Save application state here.
     };
 
-    function onResume() {
+    function onResume()
+    {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
