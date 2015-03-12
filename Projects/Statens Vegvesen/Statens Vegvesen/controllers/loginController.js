7dc3badb89a35fa24d474b465645a07c7f4ddf0c
Njål Eide <njal.eide@evry.com>
2015-03-12 15:08:14 +0100

    Fixed reading of files

---
+++ Projects/Statens Vegvesen/Statens Vegvesen/controllers/loginController.js
@@ -0,0 +1,37 @@
+﻿(function ()
+{
+    "use strict";
+
+    var app = angular.module("vegvesenApp");
+
+    app.controller("loginController", function ($scope, $location, settingsService)
+    {
+        $scope.username = "njaleide@hotmail.com";
+        $scope.password = "password";
+        $scope.authenticationError = null;
+
+        $scope.authenticate = function(userName, password) {
+            if(password == "password")
+            {
+                $scope.authenticationError = null;
+                var settings = {
+                    UserName: userName,
+                    Password: password
+                };
+
+                settingsService.SaveSettingsFile(settings, function () {
+                    $scope.$apply(function () { $location.path("/startup"); });
+                },
+                function(result) {
+                    $scope.authenticationError = "Failed to save settings: " +  result;
+                });
+                
+            }
+            else
+            {
+                $scope.authenticationError = "Invalid credentials";
+            };
+
+        };
+    });
+})();
---
+++ Projects/Statens Vegvesen/Statens Vegvesen/controllers/setupController.js
@@ -0,0 +1,55 @@
+﻿(function ()
+{
+    "use strict";
+
+    var app = angular.module("vegvesenApp");
+
+    app.controller("setupController", function ($scope, $location, settingsService)
+    {
+        document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+        function onDeviceReady()
+        {
+            // Handle the Cordova pause and resume events
+            document.addEventListener('pause', onPause.bind(this), false);
+            document.addEventListener('resume', onResume.bind(this), false);
+
+            getCredentials();
+        };
+
+        function getCredentials()
+        {
+            settingsService.ReadSettingsfile(onGetCredentials, onGetCredentialsFailed);
+        };
+
+
+        function onGetCredentials(result)
+        {
+            console.log("Retrieved credentials: " + result);
+            var settings = JSON.parse(result);
+            
+            //$scope.$apply(function () { $location.path("/startup"); });
+        }
+
+        function onGetCredentialsFailed(fileError) 
+        {
+            console.log("Failed to get credentials: " + fileError);
+            $scope.$apply(function () { $location.path("/login"); });
+        }
+
+
+    });
+    
+    function onPause()
+    {
+        // TODO: This application has been suspended. Save application state here.
+        console.log("Paused");
+    };
+
+    function onResume()
+    {
+        // TODO: This application has been reactivated. Restore application state here.
+        console.log("Resume");
+    };
+
+})();
